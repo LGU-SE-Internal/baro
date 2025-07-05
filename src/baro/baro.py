b8fcbd4194b9f32f76ccd79c6fb3dedf6c1749c2
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2025-07-05 21:33:25 +0800

    add baro from https://github.com/LGU-SE-Internal/rcabench-platform/tree/dfb9c24ff90bc97ff7c6ac99b78aa7f5ed31287b
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- src/baro/baro.py
+++ src/baro/baro.py
@@ -1,17 +1,70 @@
+from functools import partial
+
+import pandas as pd
 from rcabench_platform.v2.algorithms.spec import (
     Algorithm,
     AlgorithmAnswer,
     AlgorithmArgs,
 )
-from rcabench_platform.vendor.RCAEval.baro import baro
+
+# type:ignore
+from rcabench_platform.vendor.RCAEval.time_series import preprocess
+from sklearn.preprocessing import RobustScaler
+
 from ._common import SimpleMetricsAdapter
 
-from functools import partial
+
+def baro(
+    data: pd.DataFrame,
+    inject_time: int | None = None,
+    dataset: str | None = None,
+    anomalies: list[int] | None = None,
+    dk_select_useful: bool = False,
+):
+    if anomalies is None:
+        normal_df = data[data["time"] < inject_time]
+        anomal_df = data[data["time"] >= inject_time]
+    else:
+        normal_df = data.head(anomalies[0])
+        anomal_df = data.tail(len(data) - anomalies[0])
+
+    normal_df = preprocess(
+        data=normal_df, dataset=dataset, dk_select_useful=dk_select_useful
+    )
+
+    anomal_df = preprocess(
+        data=anomal_df, dataset=dataset, dk_select_useful=dk_select_useful
+    )
+
+    # intersect
+    intersects = [x for x in normal_df.columns if x in anomal_df.columns]
+    normal_df = normal_df[intersects]
+    anomal_df = anomal_df[intersects]
+
+    ranks = []
+
+    for col in normal_df.columns:
+        a = normal_df[col].to_numpy()
+        b = anomal_df[col].to_numpy()
+
+        scaler = RobustScaler().fit(a.reshape(-1, 1))
+        zscores = scaler.transform(b.reshape(-1, 1))[:, 0]
+        score = max(zscores)
+        ranks.append((col, score))
+
+    ranks = sorted(ranks, key=lambda x: x[1], reverse=True)
+    ranks = [x[0] for x in ranks]
+
+    return {
+        "node_names": normal_df.columns.to_list(),
+        "ranks": ranks,
+    }
+
 
 class Baro(Algorithm):
     def needs_cpu_count(self) -> int | None:
         return 4
 
     def __call__(self, args: AlgorithmArgs) -> list[AlgorithmAnswer]:
-        adapter = SimpleMetricsAdapter(partial(baro,dk_select_useful=True))
+        adapter = SimpleMetricsAdapter(partial(baro, dk_select_useful=True))
         return adapter(args)
