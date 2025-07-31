598a95900f222ef93609ff9cf70de82a9cc9b8ec
lincyaw <814750204@qq.com>
2025-07-31 20:34:48 +0800

    upgrade rcabench-platform

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 export ALGORITHM=${ALGORITHM:-baro}
-LOGURU_COLORIZE=0 .venv/bin/python run_exp.py
+LOGURU_COLORIZE=0 .venv/bin/python main.py container run
--- run_exp.py
+++ run_exp.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env -S uv run -s
-from rcabench_platform.v2.online.run_exp_platform import run
-from rcabench_platform.v2.algorithms.spec import global_algorithm_registry
+from rcabench_platform.v2.cli.main import main
 from baro.baro import Baro
+from rcabench_platform.v2.algorithms.spec import global_algorithm_registry
+
 
 if __name__ == "__main__":
     registry = global_algorithm_registry()
     registry["baro"] = Baro
-    run(enable_builtin_algorithms=False)
+
+    main(enable_builtin_algorithms=False)
+
